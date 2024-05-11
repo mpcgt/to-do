0b87e1a342603d4dc5eb400ed13d4e036a564560
Max <max.pcgt@gmail.com>
2024-05-11 18:00:05 +0200

    Update file

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,13 @@ import {ref, onMounted, computed, watch } from 'vue'
 
 const todos = ref([])
 const name = ref('')
+
+const input_content = ref('')
+const inpt_category = ref(null)
+
+const todos_asc = computed(() => todos.value.sort((a, b) => {
+  return a.createdAt - b.createdAt
+}))
 </script>
 
 <template>
