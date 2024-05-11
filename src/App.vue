a6515176ec10cf08b8295c4daa77f3575d388c67
Max <max.pcgt@gmail.com>
2024-05-11 18:03:02 +0200

    Update file

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,20 @@ const inpt_category = ref(null)
 const todos_asc = computed(() => todos.value.sort((a, b) => {
   return a.createdAt - b.createdAt
 }))
+
+watch(name, (newVal) => {
+  localStorage.setItem('name', newVal)
+})
 </script>
 
 <template>
 
+  <main class="app">
+    <section class="gretting">
+      <h2 class="title">
+        Quoi de neuf,  <input type="text" placeholder="Ton nom ici" v-model="name" />
+      </h2>
+    </section>
+  </main>
 
 </template>
